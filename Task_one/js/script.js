0a4608aff61d05cba7bb3333d2f593b42bf9cbca
tc_user <vijay.86@wipro.com>
2021-01-16 18:56:53 +0800

    virus iq initial map helper

---
+++ Task_one/js/script.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+
+    function fetchLabInfo() {
+        $(".spinner-wrapper").show()
+        $.ajax({
+			type: "POST",
+			url: "https://ncoviq.com:9090/v2/api/getLabs",
+			// The key needs to match your method's input parameter (case-sensitive).
+			data: JSON.stringify({ "state": "all" }),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(data){
+                $(".spinner-wrapper").hide();
+                displayLabs(data.response.labData);
+                initMap(data.response.labData);
+			},
+			error: function(errMsg) {
+                $(".spinner-wrapper").hide();
+				alert(errMsg);
+            }
+        });
+    }
+    fetchLabInfo()
+
+    // Initialize and add the map
+    function initMap(data) {
+        // The location of Uluru
+        const uluru = { lat: parseFloat(data[0].latitude), lng: parseFloat(data[0].longitude) };
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 4,
+            center: uluru,
+        });
+        var infowindow = new google.maps.InfoWindow();
+
+        for(let i=0; i<data.length;i+=1) {
+            let labData = data[i];
+            // The marker, positioned at Uluru
+            const marker = new google.maps.Marker({
+                position: new google.maps.LatLng(labData.latitude, labData.longitude),
+                map: map,
+            });
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    console.log($('#'+labData.labId).position().top);
+                    $("#lab-list-screen").scrollTop(0);
+                    console.log($('#'+labData.labId).position().top);
+
+                    setTimeout(function(){ 
+                        $("#lab-list-screen").animate({ scrollTop: $('#'+labData.labId).position().top + - 25 }, { duration: 'medium', easing: 'swing' });
+                    }, 100);                    
+                    infowindow.setContent(labData.managingOrganization);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+    }
+
+    function displayLabs(data) {
+        $("#lab-list-screen").empty();
+        $(".lab-count").empty();
+        $(".lab-count").append(data.length);
+        for(let i=0; i<data.length;i+=1) {
+            let labData = data[i];
+            $("#lab-list-screen").append(`<div class="card card-section" id="${labData.labId}"	>   
+                    <div class="card-body">			
+                        <h5 class="card-title">${labData.managingOrganization}</h5>
+                        <p class="card-text">${labData.serviceCapabilities}</p>			
+                        <p class="card-text">${labData.typeOfCovidTest}</p>			
+                        <p class="card-text">${labData.testTurnaround}</p>		  
+                    </div>		
+                </div>`)
+        }
+    }
+
+    $("#lab_filter_button").click(function(e){
+        $(".filter-container").toggle();
+        e.stopPropagation();
+    })
+
+    $(".filter-option-wrapper").click(function(e){
+        e.stopPropagation();
+    })
+    $(document).on("click", function () {
+        if($(".filter-container").length>0) {
+            $(".filter-container").hide();
+        }
+    });
+
+    
+
+
+});
+
+// data.response.labData
+
+// managingOrganization
+// serviceCapabilities
+// typeOfCovidTest
+// testTurnaround
+
+// latitude
+// longitude
